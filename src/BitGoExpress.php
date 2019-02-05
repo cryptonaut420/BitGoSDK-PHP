d63dfa520dfa30e2875180ccf87f6263d31bea52
cryptonaut <nickrathman@gmail.com>
2019-02-04 22:36:35 -0800

    disable ssl peer validation for localhost

--- src/BitGoExpress.php
+++ src/BitGoExpress.php
@@ -493,7 +493,7 @@ class BitGoExpress implements BitGoExpressInterface {
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array_filter($this->params)));
         }
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         if (isset($this->accessToken) && !$this->login) {
